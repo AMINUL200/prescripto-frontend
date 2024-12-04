06fc973200436936f31698b27ac40dfec6dcb5a4
Aminul Islm <ai8728340@gmail.com>
2024-12-04 13:02:14 +0530

    myappointment page update

--- src/pages/MyAppointments.jsx
+++ src/pages/MyAppointments.jsx
@@ -51,7 +51,7 @@ const MyAppointments = () => {
         const { paymentId } = data;
         const verifyData = await axios.post(backendUrl + '/api/user/verify-payment', { paymentId, appointmentId }, { headers: { token } });
         console.log(verifyData);
-        
+
         if (verifyData.data.success) {
           console.log("Verified payment");
           toast.success('Payment verified successfully');
@@ -69,6 +69,9 @@ const MyAppointments = () => {
     }
   };
 
+
+
+
   useEffect(() => {
     if (token) {
       getUserAppointments();
@@ -79,7 +82,7 @@ const MyAppointments = () => {
     <div>
       <p className='pb-3 mt-12 font-medium text-zinc-700 border-b'>My appointments</p>
       <div>
-        { appointments ?
+        {appointments.length > 0 ? (
           appointments.map((item, index) => (
             <div className='grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b' key={index}>
               <div>
@@ -91,18 +94,42 @@ const MyAppointments = () => {
                 <p className='text-zinc-700 font-medium mt-1'>Address</p>
                 <p className='text-xs'>{item.docData.address.line1}</p>
                 <p className='text-xs'>{item.docData.address.line2}</p>
-                <p className='text-sm mt-1'><span className='text-sm text-neutral-700 font-medium'>Date & Time:</span> {slotDateFormat(item.slotDate)} | {item.slotTime}</p>
+                <p className='text-sm mt-1'>
+                  <span className='text-sm text-neutral-700 font-medium'>Date & Time:</span> {slotDateFormat(item.slotDate)} | {item.slotTime}
+                </p>
               </div>
               <div className='flex flex-col gap-2 justify-end'>
-                {!item.cancelled && item.payment && !item.isCompleted && <button className='sm:min-w-48 py-2 border rounded text-stone-500 bg-indigo-50'>Paid</button>}
-                {!item.cancelled && !item.payment && !item.isCompleted && <button onClick={() => appointmentpay(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-500'>Pay Online</button>}
-                {!item.cancelled && !item.isCompleted && <button onClick={() => cancelAppointment(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-500'>Cancel appointment</button>}
-                {item.cancelled && !item.isCompleted && <button className='sm:min-w-48 py-2 border border-red-500 rounded text-red-500'>Appointment cancelled</button>}
-                {item.isCompleted && <button className='sm:min-w-48 py-2 border border-green-500 text-green-500'>Completed</button>}
+                {!item.cancelled && item.payment && !item.isCompleted && (
+                  <button className='sm:min-w-48 py-2 border rounded text-stone-500 bg-indigo-50'>Paid</button>
+                )}
+                {!item.cancelled && !item.payment && !item.isCompleted && (
+                  <button
+                    onClick={() => appointmentpay(item._id)}
+                    className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-500'
+                  >
+                    Pay Online
+                  </button>
+                )}
+                {!item.cancelled && !item.isCompleted && (
+                  <button
+                    onClick={() => cancelAppointment(item._id)}
+                    className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-500'
+                  >
+                    Cancel appointment
+                  </button>
+                )}
+                {item.cancelled && !item.isCompleted && (
+                  <button className='sm:min-w-48 py-2 border border-red-500 rounded text-red-500'>Appointment cancelled</button>
+                )}
+                {item.isCompleted && (
+                  <button className='sm:min-w-48 py-2 border border-green-500 text-green-500'>Completed</button>
+                )}
               </div>
             </div>
           ))
-         : <p className='text-center'>No appointments found</p>}
+        ) : (
+          <p className='text-center text-zinc-500 mt-4'>No appointments found</p>
+        )}
       </div>
     </div>
   );
